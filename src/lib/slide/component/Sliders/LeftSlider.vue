8541b2373272051e3fb10a93db2323a6b22e7760
leejh1316 <dlwogurtkfkd@gmail.com>
2023-12-02 06:09:08 -0500

    slider gap event fix

--- src/lib/slide/component/Sliders/LeftSlider.vue
+++ src/lib/slide/component/Sliders/LeftSlider.vue
@@ -21,41 +21,48 @@ const currentSlide = ref(0);
 const MOVE_THRESHOLD = 150;
 const VELOCITY_THRESHOLD = 3;
 
-const clampIndex = (index:number, max:number) => {
-  return Math.max(0, Math.min(index, max))
-}
+const clampIndex = (index: number, max: number) => {
+  return Math.max(0, Math.min(index, max));
+};
 const moveToSlide = (slideIndex: number) => {
-  const clampedIndex = clampIndex(slideIndex, sliderElement.value.childElementCount - 1);
+  const clampedIndex = clampIndex(
+    slideIndex,
+    sliderElement.value.childElementCount - 1
+  );
   currentCoord.value.x = -sliderElement.value.children[clampedIndex].offsetLeft;
   currentSlide.value = clampedIndex;
   endCoord.value.x = currentCoord.value.x;
   startSlideTransition();
 };
 
-
-
 /**
  * 중앙을 기점으로 index를 내뱉자
- * 
+ *
  */
- const calculateNextSlideIndex = () => {
+const calculateNextSlideIndex = () => {
   const slideItems = Array.from(sliderElement.value.children);
   let findIndex;
 
   for (const [index, slideItem] of slideItems.entries()) {
     const absEndCoordX = Math.abs(endCoord.value.x);
     const left = slideItem.offsetLeft;
-    const center = left + slideItem.offsetWidth / 2;
-    const isInside = left < absEndCoordX && absEndCoordX < (left + slideItem.offsetWidth);
+    const center = left + (slideItem.offsetWidth + props.option.gap) / 2;
+    const isInside =
+      left < absEndCoordX &&
+      absEndCoordX < left + slideItem.offsetWidth + props.option.gap;
 
     // 슬라이드가 현재 선택 범위 안에 있는지 확인
     if (isInside) {
-      const isRightSlide = directionOfSlide.value === 'right';
+      const isRightSlide = directionOfSlide.value === "right";
       // 다음 슬라이드를 선택해야 하는지 여부 결정
-      const shouldSelectNext = (isRightSlide && absEndCoordX >= center) || (!isRightSlide && absEndCoordX > center);
-      
+      const shouldSelectNext =
+        (isRightSlide && absEndCoordX >= center) ||
+        (!isRightSlide && absEndCoordX > center);
+
       // 인덱스를 범위 내로 제한
-      findIndex = shouldSelectNext ? clampIndex(index + 1, slideItems.length - 1) : index;
+      findIndex = shouldSelectNext
+        ? clampIndex(index + 1, slideItems.length - 1)
+        : index;
       break;
     }
   }
@@ -65,14 +72,14 @@ const moveToSlide = (slideIndex: number) => {
 };
 
 onSlideEnd.value = () => {
-  const isNextSlide =
-    slideVelocity.value <= -VELOCITY_THRESHOLD;
-  const isPreviousSlide =
-    slideVelocity.value >= VELOCITY_THRESHOLD;
-  if(Math.abs(moveCoord.value.x) > sliderElement.value.children[0].offsetWidth / 2){
-    moveToSlide(calculateNextSlideIndex())
-  }
-  else if (isNextSlide) {
+  const isNextSlide = slideVelocity.value <= -VELOCITY_THRESHOLD;
+  const isPreviousSlide = slideVelocity.value >= VELOCITY_THRESHOLD;
+  if (
+    Math.abs(moveCoord.value.x) >
+    sliderElement.value.children[0].offsetWidth / 2 
+  ) {
+    moveToSlide(calculateNextSlideIndex());
+  } else if (isNextSlide) {
     moveToSlide(currentSlide.value + 1);
   } else if (isPreviousSlide) {
     moveToSlide(currentSlide.value - 1);
@@ -83,20 +90,31 @@ onSlideEnd.value = () => {
 </script>
 <template>
   <div
-    ref="sliderElement"
-    class="slider"
-    :style="{
-      transform: `translateX(${currentCoord.x}px)`,
-      gap: props.option.gap + 'px',
-      top: currentCoord.y + 'px',
-    }"
+    class="slider-container"
     @mousedown="initializeSlideStart"
     @touchstart="initializeSlideStart"
   >
-    <slot></slot>
+    <div
+      ref="sliderElement"
+      class="slider"
+      :style="{
+        transform: `translateX(${currentCoord.x}px)`,
+        gap: props.option.gap + 'px',
+        top: currentCoord.y + 'px',
+      }"
+    >
+      <slot></slot>
+    </div>
   </div>
 </template>
 <style scoped lang="scss">
+.slider-container {
+  position: relative;
+  top: 0px;
+  left: 0px;
+  min-width: 100%;
+  min-height: 100%;
+}
 .slider {
   position: relative;
   top: 0px;
@@ -110,7 +128,7 @@ onSlideEnd.value = () => {
   }
 }
 @media (max-width: 380px) {
-  .slider--transition{
+  .slider--transition {
     transition: 0.2s ease all;
   }
 }
